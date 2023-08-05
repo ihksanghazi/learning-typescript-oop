70ef11f6fe4fd92d3a78b55de2121d66d9cb1235
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-05 20:52:37 +0700

    interface inheritance

---
+++ test/interface.test.ts
@@ -0,0 +1,25 @@
+describe("Interface", () => {
+	interface HasName {
+		name: string;
+	}
+
+	interface CanSayHello {
+		sayHello(name: string): void;
+	}
+
+	class Person implements HasName, CanSayHello {
+		name: string;
+		constructor(name: string) {
+			this.name = name;
+		}
+
+		sayHello(name: string): void {
+			console.info(`Hello ${name} My Name is ${this.name}`);
+		}
+	}
+
+	it("should support inheritance", () => {
+		const person = new Person("Sandy");
+		person.sayHello("Azhi");
+	});
+});
