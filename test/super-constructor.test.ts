be271abe43642f55f9a23e35cc9bd941ad0a1bfe
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-05 21:04:27 +0700

    super constructor

---
+++ test/super-constructor.test.ts
@@ -0,0 +1,22 @@
+describe("Super Constructor", () => {
+	class Person {
+		name: string;
+		constructor(name: string) {
+			this.name = name;
+		}
+	}
+
+	class Employee extends Person {
+		department: string;
+		constructor(name: string, department: string) {
+			super(name);
+			this.department = department;
+		}
+	}
+
+	it("should support", () => {
+		const employee = new Employee("Sandy", "Tech");
+		console.info(employee.name);
+		console.info(employee.department);
+	});
+});
