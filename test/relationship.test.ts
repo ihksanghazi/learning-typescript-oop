7951a5d539a0bcb1f4ecac9e5caa321455821ef4
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-06 08:42:13 +0700

    relationship

---
+++ test/relationship.test.ts
@@ -0,0 +1,13 @@
+describe("Relationship", () => {
+	class Person {
+		constructor(public name: string) {}
+	}
+	class Customer {
+		constructor(public name: string) {}
+	}
+
+	it("should support", () => {
+		const person: Person = new Customer("Sandy");
+		console.info(person.name);
+	});
+});
