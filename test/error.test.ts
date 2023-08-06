28718eff32e8244b34bbaa13c5394996b29f2c0b
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-06 08:51:18 +0700

    error handling

---
+++ test/error.test.ts
@@ -0,0 +1,25 @@
+describe("Error Handling", () => {
+	class ValidationError extends Error {
+		constructor(public message: string) {
+			super(message);
+		}
+	}
+
+	function doubleIt(value: number): number {
+		if (value < 0) {
+			throw new ValidationError("Value Cannot be less than 0");
+		}
+		return value * 2;
+	}
+
+	it("should support", () => {
+		try {
+			const result = doubleIt(-2);
+			console.info(result);
+		} catch (error) {
+			if (error instanceof ValidationError) {
+				console.info(error.message);
+			}
+		}
+	});
+});
