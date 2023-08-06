8b9086af84ffc72454fdbd02fdb0e06ff07cbca5
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-06 08:13:18 +0700

    type cast

--- test/polymorphism.test.ts
+++ test/polymorphism.test.ts
@@ -8,7 +8,27 @@ describe("Polymorphism", () => {
 	class VicePresident extends Manager {}
 
 	function sayHello(employee: Employee): void {
-		console.info(`Hello ${employee.name}`);
+		if (employee instanceof VicePresident) {
+			const vp = employee as VicePresident;
+			console.info(`Hello VP ${vp.name}`);
+		} else if (employee instanceof Manager) {
+			const manager = employee as Manager;
+			console.info(`Hello Manager ${manager.name}`);
+		} else {
+			console.info(`Hello Employee ${employee.name}`);
+		}
+	}
+
+	function sayHelloWrong(employee: Employee): void {
+		if (employee instanceof Manager) {
+			const manager = employee as Manager;
+			console.info(`Hello Manager ${manager.name}`);
+		} else if (employee instanceof VicePresident) {
+			const vp = employee as VicePresident;
+			console.info(`Hello VP ${vp.name}`);
+		} else {
+			console.info(`Hello Employee ${employee.name}`);
+		}
 	}
 
 	it("should support", () => {
@@ -26,5 +46,11 @@ describe("Polymorphism", () => {
 		sayHello(new Employee("Nur"));
 		sayHello(new Manager("Sandy"));
 		sayHello(new VicePresident("Ihksan"));
+	});
+
+	it("should support method polymorphism wrong", () => {
+		sayHelloWrong(new Employee("Nur"));
+		sayHelloWrong(new Manager("Sandy"));
+		sayHelloWrong(new VicePresident("Ihksan"));
 	});
 });
